0a168e434b920764913154aba0cd75923581786c
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2026-07-22 21:21:39 +0530

    Refactor Failure_Agent configuration and update tools list

--- failure_agent.py
+++ failure_agent.py
@@ -4,8 +4,11 @@ from vida.agents.Base_agent import Base_Agent
 from vida.utils.prompt_manager_v2 import AgentInstructionPrompt
 from tools.agent_calls import github_agent_tool_call, yaml_agent_tool_call, terraform_agent_tool_call
 from tools.probable_solutions import ProbableSolutionsAnalyzer
+from vida.utils.config import Failure_agent_config as fail_config
 
 class Failure_Agent(Base_Agent):
     name = "failure_agent"
+    model = fail_config.model
+    AI_endpoint = fail_config.AI_endpoint
     instructions = str(AgentInstructionPrompt("failure-agent-instructions"))
-    tools = [github_agent_tool_call, yaml_agent_tool_call, terraform_agent_tool_call,ProbableSolutionsAnalyzer]
+    tools = [ yaml_agent_tool_call, terraform_agent_tool_call,ProbableSolutionsAnalyzer]
